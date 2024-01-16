e85931a6fc63536ce0cc0a131f43f84824350db4
sandeep gupta <sandeepkg8756@gmail.com>
2024-01-16 20:05:25 +0530

    resume updated

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,13 +1,89 @@
 import React from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
 import { BsInfoCircleFill } from "react-icons/bs";
+import { MdWork } from "react-icons/md";
+import "react-vertical-timeline-component/style.min.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "./style.css";
+import data from "../../utils/experience";
 export default function Resume() {
   return (
     <section id="resume" className="resume">
       <PageHeaderContent
-        headerText={"Resume"}
+        headerText={"My Resume"}
         icon={<BsInfoCircleFill size={40} />}
       />
+      <div className="timeline">
+        <div className="timeline_experience">
+          <h3 className="timeline_experiece_header_text">Experience</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data?.experience?.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline_experience_vertical_timeline_element"
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-border)",
+                }}
+                date="2020 - Present"
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color",
+                }}
+              >
+                <div className="vertical_timeline_element_title_wrapper">
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
+                </div>
+                <p className="vertical_timeline_title_wrapper_description">
+                  {item.description}
+                </p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+        <div className="timeline_experience">
+          <h3 className="timeline_experiece_header_text">Education</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data?.education?.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline_experience_vertical_timeline_element"
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-border)",
+                }}
+                date="2020 - Present"
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color",
+                }}
+              >
+                <div className="vertical_timeline_element_title_wrapper">
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
+                </div>
+                {/* <p className="vertical_timeline_element_title_wrapper_description">
+                  {item.description}
+                </p> */}
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
     </section>
   );
 }
---
+++ src/utils/experience.jsx
@@ -0,0 +1,34 @@
+const data = {
+  experience: [
+    {
+      title: "Mern stack dveloper",
+      subTitle: "pune mumbai",
+      description: "User Experience",
+    },
+    {
+      title: "Mern stack developer",
+      subTitle: "mumbai",
+      description: "Web development,database,backend",
+    },
+  ],
+
+  education: [
+    {
+      title: "Secondary",
+      subTitle: "Public Inter College Motiha Prayagraj Uttarpradesh",
+      description: "10th Grade",
+    },
+    {
+      title: "Higher Secondary",
+      subTitle: "Public Inter College Motiha Prayagraj Uttarpradesh",
+      description: "12th Grade in science",
+    },
+    {
+      title: "Graducation",
+      subTitle: "United College of engineering and research prayagraj",
+      description: "BTECH in Computer Science",
+    },
+  ],
+};
+
+export default data;
